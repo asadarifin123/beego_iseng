3b098bf96a583548c766cc6dd8607edaddcb1a87
M Asad Arifin H <17523041@students.uii.ac.id>
2020-11-22 17:22:29 +0700

    pertama

---
+++ controllers/default.go
@@ -0,0 +1,114 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type MainController struct {
+	beego.Controller
+}
+
+type IndexController struct {
+	beego.Controller
+}
+
+type PostmortemController struct {
+	beego.Controller
+}
+
+type LoginController struct {
+	beego.Controller
+}
+
+type DashboardController struct{
+	beego.Controller
+}
+
+type DashPostController struct{
+	beego.Controller
+}
+
+type DashPagesController struct{
+	beego.Controller
+}
+
+type DashWebController struct{
+	beego.Controller
+}
+
+type DashSSLController struct{
+	beego.Controller
+}
+
+type DashCVEController struct{
+	beego.Controller
+}
+
+type DashBackupController struct{
+	beego.Controller
+}
+
+type DashUserController struct{
+	beego.Controller
+}
+
+func (c *MainController) Get() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.TplName = "index.tpl"
+}
+
+func (c *IndexController) Get() {
+	c.Data["message"] = "HOME"
+	c.TplName = "index.html"
+}
+
+func (c *PostmortemController) Get() {
+	c.Data["message"] = "POSTMORTEM"
+	c.TplName = "postmortem.html"
+}
+
+func (c *LoginController) Get() {
+	c.Data["message"] = "LOGIN"
+	c.TplName = "login.html"
+}
+
+func (c *DashboardController) Get() {
+	c.Data["message"] = "DASHBOARD"
+	c.TplName = "admin/dashboard.html"
+}
+
+func (c *DashPostController) Get() {
+	c.Data["message"] = "Postmortem"
+	c.TplName = "admin/dashpost.html"
+}
+
+func (c *DashPagesController) Get() {
+	c.Data["message"] = "Pages"
+	c.TplName = "admin/dashpages.html"
+}
+
+func (c *DashWebController) Get() {
+	c.Data["message"] = "Web Monitoring"
+	c.TplName = "admin/dashweb.html"
+}
+
+func (c *DashSSLController) Get() {
+	c.Data["message"] = "SSL Checker"
+	c.TplName = "admin/dashssl.html"
+}
+
+func (c *DashCVEController) Get() {
+	c.Data["message"] = "CVE Data"
+	c.TplName = "admin/dashcve.html"
+}
+
+func (c *DashBackupController) Get() {
+	c.Data["message"] = "Web Backup"
+	c.TplName = "admin/dashbackup.html"
+}
+
+func (c *DashUserController) Get() {
+	c.Data["message"] = "User"
+	c.TplName = "admin/dashuser.html"
+}
---
+++ routers/router.go
@@ -0,0 +1,21 @@
+package routers
+
+import (
+	"iseng/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+    beego.Router("/index", &controllers.IndexController{})
+    beego.Router("/postmortem", &controllers.PostmortemController{})
+    beego.Router("/login", &controllers.LoginController{})
+    beego.Router("/dashboard", &controllers.DashboardController{})
+    beego.Router("/dashboard/postmortem", &controllers.DashPostController{})
+    beego.Router("/dashboard/pages", &controllers.DashPagesController{})
+    beego.Router("/dashboard/web", &controllers.DashWebController{})
+    beego.Router("/dashboard/ssl", &controllers.DashSSLController{})
+    beego.Router("/dashboard/cve", &controllers.DashCVEController{})
+    beego.Router("/dashboard/backup", &controllers.DashBackupController{})
+    beego.Router("/dashboard/user", &controllers.DashUserController{})
+}
